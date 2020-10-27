5989ff6be142365324eac6fdb229cd8480b915f5
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-10-27 11:02:22 +0100

    Added Shipping Calculator app

---
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -0,0 +1,10 @@
+package com.redhat.shipping;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShippingCalculatorTest {
+
+    // @todo: add tests
+}
