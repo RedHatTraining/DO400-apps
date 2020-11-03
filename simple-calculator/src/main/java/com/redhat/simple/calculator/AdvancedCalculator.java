c68bc9717abceded81b8ff38904b6370c19d193c
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-11-03 14:15:53 +0100

    Renamed variables

--- simple-calculator/src/main/java/com/redhat/simple/calculator/AdvancedCalculator.java
+++ simple-calculator/src/main/java/com/redhat/simple/calculator/AdvancedCalculator.java
@@ -3,24 +3,24 @@ package com.redhat.simple.calculator;
 public final class AdvancedCalculator {
     static final double PI = 3.14;
 
-    public int divide(final int n, final int d) {
-        if (d == 0) {
+    public int divide(final int dividend, final int divisor) {
+        if (divisor == 0) {
             return Integer.MAX_VALUE;
         } else {
-            return n / d;
+            return dividend / divisor;
         }
     }
 
-    public int subs(final int a, final int b) {
-        return a - b;
+    public int subs(final int minuend, final int subtrahend) {
+        return minuend - subtrahend;
     }
 
-    public int sum(final int a, final int b) {
-        return a + b;
+    public int sum(final int addendA, final int addendB) {
+        return addendA + addendB;
     }
 
-    public int multiply(final int a, final int b) {
-        return a * b;
+    public int multiply(final int multiplicand, final int multiplier) {
+        return multiplicand * multiplier;
     }
 
     public double circumference(final int r) {
