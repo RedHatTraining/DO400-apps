c0e24d26d5212cfd93115a2853ec19838a422c1b
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-11-03 11:41:46 +0100

    Renamed class

--- simple-calculator/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
+++ simple-calculator/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
@@ -2,13 +2,7 @@ package com.redhat.simple.calculator;
 
 import java.util.*;
 
-public class Calculator {
-    static final double pi = 3.14;
-
-    public double circumference(int r) {
-        return 2*pi*r;
-    }
-
+public class BasicCalculator {
     public int divide(int n, int d) {
         if (d == 0)
             return Integer.MAX_VALUE;
@@ -16,17 +10,21 @@ public class Calculator {
             return n / d;
     }
 
+    public int subs(int a, int b) {
+        return a - b;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
     public int random() {
         Random r = new Random();
 
         return r.nextInt();
-    }
-
-    public boolean is_greater_than(int a, int b) {
-        if (a > b) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
