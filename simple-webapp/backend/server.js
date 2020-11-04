87ad3cc2ca8742fc5a54ef2169129ec1ed36866f
Jaime Ramirez <jaime.ramirez@redhat.com>
2020-11-04 10:22:01 +0100

    refactor

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -4,10 +4,12 @@ const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
+    const greeting = greet(name);
+
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greet(name));
+    res.end(greeting);
 });
 
 const port = 3000;
