058562aec52bed7221ea9a4c833f0f7aaa2609f5
Jaime Ramirez <jaime.ramirez@redhat.com>
2020-11-04 14:57:05 +0100

    Revert "refactor"
    This reverts commit 87ad3cc2ca8742fc5a54ef2169129ec1ed36866f.

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -4,12 +4,10 @@ const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
-    const greeting = greet(name);
-
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greeting);
+    res.end(greet(name));
 });
 
 const port = 3000;
