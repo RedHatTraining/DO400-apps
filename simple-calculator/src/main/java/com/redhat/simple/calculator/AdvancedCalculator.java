170bf59fae9d790d690130c161d2b9e91f1ef534
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-11-05 09:34:30 +0100

    Solutions for ch04s08

--- simple-calculator/src/main/java/com/redhat/simple/calculator/AdvancedCalculator.java
+++ simple-calculator/src/main/java/com/redhat/simple/calculator/AdvancedCalculator.java
@@ -1,6 +1,6 @@
 package com.redhat.simple.calculator;
 
-public final class AdvancedCalculator {
+public final class AdvancedCalculator extends Calculator {
     static final double PI = 3.14;
     static final double LOW_DISCOUNT_AMOUNT = 0.10;
     static final double MID_DISCOUNT_AMOUNT = 0.15;
@@ -13,26 +13,6 @@ public final class AdvancedCalculator {
     static final int SILVER_CUSTOMER_SEGMENT = 2;
     static final int GOLD_CUSTOMER_SEGMENT = 3;
 
-    public int divide(final int dividend, final int divisor) {
-        if (divisor == 0) {
-            return Integer.MAX_VALUE;
-        } else {
-            return dividend / divisor;
-        }
-    }
-
-    public int subs(final int minuend, final int subtrahend) {
-        return minuend - subtrahend;
-    }
-
-    public int sum(final int addendA, final int addendB) {
-        return addendA + addendB;
-    }
-
-    public int multiply(final int multiplicand, final int multiplier) {
-        return multiplicand * multiplier;
-    }
-
     public double circumference(final int r) {
         return 2 * PI * r;
     }
@@ -44,44 +24,60 @@ public final class AdvancedCalculator {
     public double discount(final double saleAmount,
                            final int yearsAsCustomer,
                            final boolean isBusiness) {
-        double discount = 0;
-
         if (saleAmount < MINIMUM_PURCHASE_AMOUNT
                 || yearsAsCustomer < BRONZE_CUSTOMER_SEGMENT) {
             return 0;
         }
 
         if (saleAmount < LOW_PURCHASE_AMOUNT) {
-            if (isBusiness) {
-                discount = LOW_DISCOUNT_AMOUNT;
-            } else {
-                if (yearsAsCustomer > GOLD_CUSTOMER_SEGMENT) {
-                    discount = MID_DISCOUNT_AMOUNT;
-                } else {
-                    discount = LOW_DISCOUNT_AMOUNT;
-                }
-            }
+            return discountForLowPurchases(yearsAsCustomer, isBusiness);
         } else if (saleAmount > HIGH_PURCHASE_AMOUNT) {
-            if (isBusiness) {
+            return discountForHighPurchases(yearsAsCustomer, isBusiness);
+        }
+
+        return 0;
+    }
+
+    private double discountPerCustomerSegment(final int yearsAsCustomer) {
+        double discount;
+
+        switch (yearsAsCustomer) {
+            case BRONZE_CUSTOMER_SEGMENT:
+                discount = LOW_DISCOUNT_AMOUNT;
+                break;
+            case SILVER_CUSTOMER_SEGMENT:
+                discount = MID_DISCOUNT_AMOUNT;
+                break;
+            case GOLD_CUSTOMER_SEGMENT:
+                discount = HIGH_DISCOUNT_AMOUNT;
+                break;
+            default:
                 discount = VIP_DISCOUNT_AMOUNT;
-            } else {
-                switch (yearsAsCustomer) {
-                    case BRONZE_CUSTOMER_SEGMENT:
-                        discount = LOW_DISCOUNT_AMOUNT;
-                        break;
-                    case SILVER_CUSTOMER_SEGMENT:
-                        discount = MID_DISCOUNT_AMOUNT;
-                        break;
-                    case GOLD_CUSTOMER_SEGMENT:
-                        discount = HIGH_DISCOUNT_AMOUNT;
-                        break;
-                    default:
-                        discount = VIP_DISCOUNT_AMOUNT;
-                        break;
-                }
-            }
+                break;
         }
 
         return discount;
+    }
+
+    private double discountForLowPurchases(final int yearsAsCustomer,
+                                           final boolean isBusiness) {
+        if (isBusiness) {
+            return LOW_DISCOUNT_AMOUNT;
+        }
+
+        if (yearsAsCustomer > GOLD_CUSTOMER_SEGMENT) {
+            return MID_DISCOUNT_AMOUNT;
+        }
+
+        return LOW_DISCOUNT_AMOUNT;
+    }
+
+    private double discountForHighPurchases(final int yearsAsCustomer,
+                                            final boolean isBusiness) {
+        if (isBusiness) {
+            return VIP_DISCOUNT_AMOUNT;
+        }
+
+        return discountPerCustomerSegment(yearsAsCustomer);
     }
 }
--- simple-calculator/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
+++ simple-calculator/src/main/java/com/redhat/simple/calculator/BasicCalculator.java
@@ -1,30 +1,12 @@
 package com.redhat.simple.calculator;
 
-import java.util.*;
+import java.util.Random;
 
-public class BasicCalculator {
-    public int divide(int dividend, int divisor) {
-        if (divisor == 0)
-            return Integer.MAX_VALUE;
-        else
-            return dividend / divisor;
-    }
+public final class BasicCalculator extends Calculator {
 
-    public int subs(int minuend, int subtrahend) {
-        return minuend-subtrahend;
-    }
+    private final Random randomService = new Random();
 
-    public int sum(int addendA, int addendB) {
-        return addendA+addendB;
-    }
-
-    public int multiply(int multiplicand, int multiplier) {
-        return multiplicand*multiplier;
-    }
-
-    public int Random() {
-        Random r = new Random();
-
-        return r.nextInt();
+    public int random() {
+        return randomService.nextInt();
     }
 }
