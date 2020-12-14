0303070d82c9e1920788007d1491af85649632e5
Pablo Solar Vilariño <psolarvi@redhat.com>
2020-12-14 14:35:31 +0100

    Added namespace

--- tools/check-job-state.sh
+++ tools/check-job-state.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-oc logs -f jobs/${1} && \
-podState=$(oc get pods --selector=job-name=${1} \
+oc wait --for=condition=ContainersReady \
+  --timeout=60s pod --selector job-name=${1} -n ${2}
+
+oc logs -f jobs/${1} -n ${2} && \
+podState=$(oc get pods --selector=job-name=${1} -n ${2} \
   --output=jsonpath='{.items[0].status.containerStatuses[0].state.terminated.exitCode}')
 
 if [ "${podState}" -eq 0 ]
