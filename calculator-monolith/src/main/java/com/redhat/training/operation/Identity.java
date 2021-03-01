e5788478bb52e570bee28727b2e593b552cc2eab
Jordi Sola <someth2say@gmail.com>
2021-03-01 17:19:49 +0100

    Remove unneeded imports

--- calculator-monolith/src/main/java/com/redhat/training/operation/Identity.java
+++ calculator-monolith/src/main/java/com/redhat/training/operation/Identity.java
@@ -1,12 +1,5 @@
 package com.redhat.training.operation;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
